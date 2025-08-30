86c5d633ba2ef83e4693819cc1437dfcaa04dc21
Maik Bialas <maik.bialas@ernercity-netz.de>
2025-08-30 16:42:40 +0200

    ecnore-demo

---
+++ airflow/dags/encore-workflow-demo/encore-workflow-demo-dag.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+import os
+from airflow import DAG
+from airflow.utils import timezone
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+
+_THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_FILE = "spark-postgres-extractor.yaml"
+
+
+with DAG(
+    dag_id="encore-workflow-demo",
+    start_date=timezone.datetime(2025, 8, 24, tzinfo=timezone.utc),
+    schedule=None,                              # keep None while testing conf
+    catchup=False,
+    render_template_as_native_obj=True,         # preserve int/bool types
+    params={                                    # defaults (overridden by dag_run.conf)
+        "spark_namespace": "spark-operator",
+        "spark_image": "mabi/encore-data-loader:latest",
+        "executor_instances": 1,
+        "main_file": "local:///opt/app/encore-workflow-demo-dag.py",
+        "APP_NAME": "encore-workflow-demo",
+        
+        "USERNAME": "postgres",
+        "PASSWORD": "mysecretpassword",
+    },
+    tags=["spark", "kubernetes", "spark-operator", "encore"],
+) as dag:
+    
+
+    load_data = SparkKubernetesOperator(
+        task_id="load_data",
+        application_file=APP_FILE,   # resolves via template_searchpath
+        namespace="{{ dag_run.conf.get('spark_namespace', params.spark_namespace) }}",
+        kubernetes_conn_id="kubernetes_default",
+        do_xcom_push=False,
+    )
+
+    load_data
---
+++ airflow/dags/encore-workflow-demo/spark-data-loader/load_postgres_data.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+import os
+
+# Pull credentials from environment
+pg_user = os.environ["USER"]      # New: user from env var
+pg_password = os.environ["PASSWORD"]
+
+# JDBC URL to Postgres service in AKS
+url = "jdbc:postgresql://postgres.db.svc.cluster.local:5432/db"
+
+# Connection properties
+props = {
+    "user": pg_user,
+    "password": pg_password,
+    "driver": "org.postgresql.Driver",
+    "ssl": "false"
+}
+
+spark = SparkSession.builder \
+    .appName("ReadPostgresWidgets") \
+    .master("local[*]") \
+    .getOrCreate()
+
+df = spark.read.jdbc(url=url, table="widgets", properties=props)
+print("Row count:", df.count())
+df.show(truncate=False)   # print the rows to stdout
+
+# Example: do something minor and write back as a demo
+out = df.selectExpr("id", "upper(name) as name_upper", "created_at")
+out.show(truncate=False)
+
+spark.stop()
